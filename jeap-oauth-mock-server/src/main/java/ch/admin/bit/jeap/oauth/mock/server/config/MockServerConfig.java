a8f5f03928e48cbded57057d87beda2d34566262
Matthias Fritschi <matthias.fritschi@bit.admin.ch>
2025-01-31 11:11:46 +0100

    JEAP-5177 Remove static keystore

--- jeap-oauth-mock-server/src/main/java/ch/admin/bit/jeap/oauth/mock/server/config/MockServerConfig.java
+++ jeap-oauth-mock-server/src/main/java/ch/admin/bit/jeap/oauth/mock/server/config/MockServerConfig.java
@@ -1,11 +1,10 @@
 package ch.admin.bit.jeap.oauth.mock.server.config;
 
+import jakarta.annotation.PostConstruct;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
-
-import jakarta.annotation.PostConstruct;
 
 /**
  * General configuration of the mock-server instance
@@ -16,22 +15,6 @@ import jakarta.annotation.PostConstruct;
 @Slf4j
 public
 class MockServerConfig {
-    /**
-     * Name of the keystore file to use, normally this should not be changed
-     */
-    private String jwtKeystoreName = "/mockserver.jks";
-    /**
-     * Secret of the keystore file to use, normally this should not be changed
-     */
-    private String jwtKeystoreSecret = "secret";
-    /**
-     * Alias of the key in the keystore file to use, normally this should not be changed
-     */
-    private String jwtKeypairAlias = "mockserver";
-    /**
-     * Secret of the key in the keystore file to use, normally this should not be changed
-     */
-    private String jwtKeypairSecret = "secret";
     /**
      * Base-URL of where the oauth mock server is reachable. Change this e.g. if you change the port, hostname of path
      */
